cdacbec2bc4e4780425ffa58ab6067844203eaa3
ddarrkkaa <118774639+ddarrkkaa@users.noreply.github.com>
2023-03-19 21:28:58 +0200

    #2

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -7,21 +7,47 @@
             Console.WriteLine("Enter the sentence: ");
             List<string> Sentence1 = new List<string>(); 
             string sent = Console.ReadLine();
-            Sentence1 = sent.Split(",").ToList();
+            Sentence1 = sent. Split(' ').ToList();
+
             Sentence sentence = new Sentence();
             
-            Console.WriteLine("Enter the word you`d like to add: "); //adding
+            Console.WriteLine("Enter the word you`d like to add: "); 
             string word1 = Console.ReadLine();
-            sentence.AddWord(Sentence1, word1);  //злипається останнє слово
+            sentence.AddWord(Sentence1, word1);  
             
-            Console.WriteLine("Enter the number of the word you`d like to delete: "); //deleting
+            Console.WriteLine("Enter the number of the word you`d like to delete: "); 
             int numb1 = Int32.Parse(Console.ReadLine());
             numb1 -= 1;
             sentence.DeleteWord(Sentence1, numb1);
             
+            Console.WriteLine("Enter the number of the word you`d like to insert: ");
             int numb2 = Int32.Parse(Console.ReadLine());
+            numb2 -= 1;
+            Console.WriteLine("Enter the word you`d like to insert: ");
             string word2 = Console.ReadLine();
-            sentence.InsertWord(Sentence1, numb2, word2);
+            sentence.InsertWord(Sentence1, numb2, word2); 
+
+            sentence.CountOfLetters(Sentence1);
+            
+            sentence.CountOfWords(Sentence1);
+            
+            sentence.TheLongest(Sentence1);
+            
+            sentence.TheShortest(Sentence1);
+            
+            Console.WriteLine("Enter the word you`d like to check: "); 
+            string word3 = Console.ReadLine();
+            sentence.ExistTheWord(Sentence1,word3);
+            
+            Console.WriteLine("Enter the number of the word you`d like to find: ");
+            int numb4 = Int32.Parse(Console.ReadLine());
+            sentence.FindNumb(Sentence1,numb4);
+            
+            Console.WriteLine("Enter second sentence: ");
+            List<string> Sentence2 = new List<string>(); 
+            string sent2 = Console.ReadLine();
+            Sentence2 = sent2. Split(' ').ToList();
+            sentence.Equal(Sentence1,Sentence2);
         }
     }
 }
--- Lab3/Sentence.cs
+++ Lab3/Sentence.cs
@@ -7,7 +7,7 @@ namespace Lab3
             sentence.Add(word);
             foreach (string element in sentence) 
             {
-                Console.Write(element);
+                Console.WriteLine(element);
             }
         }
         public void DeleteWord(List<string> sentence, int numb)
@@ -21,15 +21,55 @@ namespace Lab3
         public void InsertWord(List<string> sentence,int numb, string word)
         {
             sentence.Insert(numb,word);
-            foreach (string elementr in sentence) 
+            foreach (string element in sentence) 
             {
-                Console.Write(elementr);
+                Console.WriteLine(element);
             }                                                        
         }
 
-        public void CountOfLetters(List<string> se)
+        public void CountOfLetters(List<string> sentence)
         {
-            
+            List<char> charList = sentence.SelectMany(str => str.ToCharArray()).ToList();
+            Console.WriteLine($"The count of lrtters is: {charList.Count()}");
+        }
+        public void CountOfWords(List<string> sentence)
+        {
+            Console.WriteLine($"The count of words is: {sentence.Count()}");
+        }
+
+        public void TheLongest(List<string> sentence)
+        {
+            string longestWord = sentence.OrderByDescending(s => s.Length).FirstOrDefault();
+            Console.WriteLine($"The longest word is: {longestWord}");
+        }
+        public void TheShortest(List<string> sentence)
+        {
+            string shortestWord = sentence.OrderByDescending(s => s.Length).LastOrDefault();
+            Console.WriteLine($"The shortest word is: {shortestWord}");
+        }
+
+        public void ExistTheWord(List<string> sentence, string word)
+        {
+            bool exist = sentence.Contains(word);
+            if (exist==true)
+                Console.WriteLine("This word is in the sentence");
+            else
+                Console.WriteLine("This word is not in the sentence");
+        }
+
+        public void FindNumb(List<string> sentence, int numb)
+        {
+            int numb2 = numb-1;
+            Console.WriteLine($"The word {numb} is {sentence[numb2]}");
+        }
+
+        public void Equal(List<string> sentence, List<string> sentence2)
+        {
+            //bool eq = false;
+            if(sentence.Count()==sentence2.Count())
+                Console.WriteLine("the sentences are equal");
+            else
+                Console.WriteLine("the sentences aren`t equal");
         }
     }
 }
